8cc663810db3fc5fd308adc37e8ea1cd469d0840
M1dDler <m1ddler.ua@gmail.com>
2024-05-18 13:58:01 +0300

    Add custom TCPConnector

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiohttp
 from API.api import server_run
 from TelegramBot.bot import bot
 import signal
@@ -8,5 +9,10 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-server_run()
-asyncio.run(bot.polling(non_stop=True, request_timeout=90))
+async def main():
+    connector = aiohttp.TCPConnector(force_close=True)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        server_run()
+        await bot.polling(non_stop=True)
+
+asyncio.run(main())
